d0cb42be016bf90b1bafd6648fbffdc862f35bdd
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-01-30 12:46:35 +0000

    Update provider-variables.tf

--- provider-variables.tf
+++ provider-variables.tf
@@ -25,3 +25,15 @@ variable "gcp_zone" {
   type        = string
   description = "GCP zone"
 }
+
+variable "gcp_private_key" { 
+  type = string 
+} 
+
+variable "gcp_cred" { 
+  type = map 
+} 
+
+locals {
+  credential = merge(var.gcp_cred, {private_key = "${var.gcp_private_key}"}) 
+}
