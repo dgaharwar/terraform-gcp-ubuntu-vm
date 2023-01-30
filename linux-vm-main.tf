55babcfdd891214ff952b54ded4dd3c81cb260f0
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-01-30 12:52:32 +0000

    Update linux-vm-main.tf

--- linux-vm-main.tf
+++ linux-vm-main.tf
@@ -14,6 +14,10 @@ sudo apt-get update;
 sudo apt-get install -yq build-essential apache2;
 sudo systemctl start apache2;
 sudo systemctl enable apache2;
+  #cloud-config
+runcmd:
+- <%=instance.cloudConfig.agentInstall%>
+- <%=instance.cloudConfig.finalizeServer%>
 EOF
 }
 
