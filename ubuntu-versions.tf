2d0c5c5491dfd85f80adf2df154da05b777d536e
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-02-15 13:00:35 +0000

    Update ubuntu-versions.tf

--- ubuntu-versions.tf
+++ ubuntu-versions.tf
@@ -10,7 +10,7 @@
 variable "ubuntu_2004_sku" {
   type        = string
   description = "SKU for Ubuntu 20.04 LTS"
-  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
+  default     = "ubuntu-2004-focal-v20230113"
 }
 #variable "ubuntu_pro_1604_sku" {
 #  type        = string
